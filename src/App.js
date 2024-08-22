86f0db93c7a072afb36101ab49df866cc05ae52c
Mike Noired <unkstd@mail.ru>
2024-08-22 15:41:19 +0300

    Add bullets w/ speed changing & player score

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import { useCallback, useEffect, useMemo, useRef } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 
 const App = () => {
   const canvasRef = useRef(null);
   const cursorRef = useRef(() => ({ x: 0, y: 0 }), []);
+  const [playerShots, setPlayerShots] = useState({
+    1: 0,
+    2: 0,
+  });
   const players = useMemo(
     () => [
-      { id: 1, x: 50, y: 20, radius: 20, color: "red", dy: 3 },
-      { id: 2, x: 450, y: 480, radius: 20, color: "blue", dy: 3 },
+      {
+        id: 1,
+        x: 50,
+        y: 20,
+        radius: 20,
+        color: "red",
+        dy: 3,
+        bullets: [],
+        bulletSpeed: 5,
+      },
+      {
+        id: 2,
+        x: 450,
+        y: 480,
+        radius: 20,
+        color: "blue",
+        dy: 3,
+        bullets: [],
+        bulletSpeed: 5,
+      },
     ],
     [],
   );
@@ -22,6 +44,7 @@ const App = () => {
 
   const draw = useCallback(
     (ctx, can) => {
+      let bulletIntervalId;
       const onMouseMove = (event) => getMousePos(can, event);
 
       const drawPlayers = () => {
@@ -53,6 +76,66 @@ const App = () => {
         });
       };
 
+      const drawBullets = () => {
+        players.forEach((player) => {
+          player.bullets.push({
+            x: player.x,
+            y: player.y,
+            radius: 5,
+            color: "black",
+          });
+        });
+      };
+
+      const hasShot = (bullet, player) => {
+        const dx = bullet.x - player.x;
+        const dy = bullet.y - player.y;
+        const distance = Math.sqrt(dx ** 2 + dy ** 2);
+        return distance <= player.radius + bullet.radius;
+      };
+
+      const moveBullet = (bullet, player, enemy) => {
+        const speed = player.bulletSpeed;
+        const newX = player.id === 1 ? bullet.x + speed : bullet.x - speed;
+
+        if (newX > can.width || newX < 0) return false;
+
+        const hasBulletHitEnemy = hasShot(bullet, enemy);
+
+        if (hasBulletHitEnemy) {
+          setPlayerShots((prevPlayerShots) => ({
+            ...prevPlayerShots,
+            [player.id]: prevPlayerShots[player.id] + 1,
+          }));
+          return false;
+        }
+
+        return { ...bullet, x: newX };
+      };
+
+      const handleBulletCollision = (player, enemy) => {
+        player.bullets = player.bullets.map((bullet) =>
+          moveBullet(bullet, player, enemy),
+        );
+      };
+
+      const bulletAction = (playerIndex, enemyIndex) => {
+        handleBulletCollision(players[playerIndex], players[enemyIndex]);
+      };
+
+      const updateBullets = () => {
+        bulletAction(0, 1);
+        bulletAction(1, 0);
+        players.forEach((player) => {
+          player.bullets.forEach((bullet) => {
+            ctx.beginPath();
+            ctx.arc(bullet.x, bullet.y, 5, 0, 2 * Math.PI);
+            ctx.fillStyle = "black";
+            ctx.fill();
+          });
+        });
+      };
+
       can.addEventListener("mousemove", onMouseMove);
 
       drawPlayers();
@@ -61,6 +144,11 @@ const App = () => {
       const animate = () => {
         drawPlayers();
         updatePlayers();
+        updateBullets();
+        if (bulletIntervalId) {
+          clearInterval(bulletIntervalId);
+        }
+        bulletIntervalId = setInterval(drawBullets, 1000);
         requestId = window.requestAnimationFrame(animate);
       };
       animate();
@@ -82,10 +170,22 @@ const App = () => {
       <canvas ref={canvasRef} id="canvas" width={500} height={500} />
       <div className="rangeControls">
         <div>
-          <span>Player 1</span>
+          <span>
+            Player 1 <b>{playerShots[1]}</b>
+          </span>
           <label htmlFor="player1_bulspeed">
             Bullet speed:
-            <input name="player1" id="bullet" type="range" min={1} max={10} />
+            <input
+              defaultValue={players[0].bulletSpeed}
+              onChange={(e) => {
+                players[0].bulletSpeed = parseInt(e.target.value, 10);
+              }}
+              name="player1"
+              id="bullet"
+              type="range"
+              min={1}
+              max={10}
+            />
           </label>
           <label htmlFor="player1_bulspeed">
             Player speed:
@@ -104,10 +204,22 @@ const App = () => {
           </label>
         </div>
         <div>
-          <span>Player 2</span>
+          <span>
+            Player 2 <b>{playerShots[2]}</b>
+          </span>
           <label htmlFor="player2_bulspeed">
             Bullet speed:
-            <input name="player2" id="bullet" type="range" min={1} max={10} />
+            <input
+              defaultValue={players[1].bulletSpeed}
+              onChange={(e) => {
+                players[1].bulletSpeed = parseInt(e.target.value, 10);
+              }}
+              name="player2"
+              id="bullet"
+              type="range"
+              min={1}
+              max={10}
+            />
           </label>
           <label htmlFor="player2_bulspeed">
             Player speed:
